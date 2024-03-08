7084c12c2f1aa400933a210fcef97431aca00a60
ftalamo <francotalamo@gmail.com>
2024-03-08 20:32:55 -0300

    add unzip function

---
+++ src/unzip.py
@@ -0,0 +1,7 @@
+import  zipfile
+
+
+def unzip_file(zip_file, extract_to):
+    with zipfile.ZipFile(zip_file, 'r') as zip_ref:
+        zip_ref.extractall(extract_to)
+        return zip_ref.infolist()
