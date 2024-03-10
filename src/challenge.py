788d2fbd0393df3a24a3b33870b8846a4fd75f4f
ftalamo <francotalamo@gmail.com>
2024-03-10 18:19:09 -0300

    create q3_time function, add prints to challenge

--- src/challenge.py
+++ src/challenge.py
@@ -5,6 +5,8 @@ from q1_memory import q1_memory
 from q1_time import q1_time
 from q2_memory import q2_memory
 from q2_time import q2_time
+from q3_memory import q3_memory
+from q3_time import q3_time
 
 
 if __name__ == "__main__":
@@ -13,25 +15,34 @@ if __name__ == "__main__":
     data_dir = 'data/'
     file_id = '1ig2ngoXFTxP5Pa8muXo02mDTFexZzsis'
 
+    '''verifica que exista el archivo antes de ir a buscarlo en drive'''
     datafile = 'data/farmers-protest-tweets-2021-2-4.json'
-
     if os.path.exists(datafile):
         '''si existe el archivo copia el nombre'''
         jdfile_name = datafile
     else:
         '''descarga el archivo y lo descomprime desde drive'''
-        datafile = getdata(file_id, credentials, data_dir)
-        jsondatafile = unzip_file(data_dir + datafile, data_dir)
-        jdfile_name = data_dir + jsondatafile[0].filename
+        try:
+            datafile = getdata(file_id, credentials, data_dir)
+            jsondatafile = unzip_file(data_dir + datafile, data_dir)
+            jdfile_name = data_dir + jsondatafile[0].filename
+        except Exception as e:
+            print(f"error: {e}")
+            print('Por favor ve al link https://drive.usercontent.google.com/download?id=1ig2ngoXFTxP5Pa8muXo02mDTFexZzsis&export=download&authuser=0 y descarga de forma manual el archivo')
 
-    #q1_memory_optimize = q1_memory(jdfile_name)
-    #print(q1_memory_optimize)
-    #q1_time_optimize = q1_time(jdfile_name)
-    #print(q1_time_optimize)
+    q1_memory_optimize = q1_memory(jdfile_name)
+    print(f"q1_memory result: {q1_memory_optimize}")
+    q1_time_optimize = q1_time(jdfile_name)
+    print(f"q1_time result: {q1_time_optimize}")
     q2_memory_optimize = q2_memory(jdfile_name)
-    print (q2_memory_optimize)
+    print(f"q2_memory result: {q2_memory_optimize}")
     q2_time_optimize = q2_time(jdfile_name)
-    print(q2_time_optimize)
+    print(f"q2_time result: {q2_time_optimize}")
+    q3_memory_optimize = q3_memory(jdfile_name)
+    print(f"q3_memory result: {q3_memory_optimize}")
+    q3_time_optimize = q3_time(jdfile_name)
+    print(f"q3_time result: {q3_time_optimize}")
+
 
 
 
--- src/q3_time.py
+++ src/q3_time.py
@@ -1,4 +1,30 @@
+import multiprocessing
 from typing import List, Tuple
+from collections import Counter
+from Readjson import read_json
+from memory_profiler import profile
+
+
+def process_tweet(tweet):
+    mentioned_counter = Counter()
+    if 'mentionedUsers' in tweet:
+        mentioned_users = tweet['mentionedUsers']
+        if mentioned_users:
+            for user in mentioned_users:
+                username = user.get('username')
+                if username:
+                    mentioned_counter[username] += 1
+    return mentioned_counter
 
 def q3_time(file_path: str) -> List[Tuple[str, int]]:
-    pass
+    data = read_json(file_path)
+    mentioned_counter = Counter()
+
+    with multiprocessing.Pool() as pool:
+        results = pool.map(process_tweet, data)
+
+    for counter in results:
+        mentioned_counter.update(counter)
+
+    top_10 = mentioned_counter.most_common(10)
+    return top_10
