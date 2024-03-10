7b14e999bac685efa8777f993bbc5b77dc34d0e1
ftalamo <francotalamo@gmail.com>
2024-03-10 17:29:51 -0300

    add q2_time optimize function

--- src/challenge.py
+++ src/challenge.py
@@ -4,6 +4,7 @@ from unzip import unzip_file
 from q1_memory import q1_memory
 from q1_time import q1_time
 from q2_memory import q2_memory
+from q2_time import q2_time
 
 
 if __name__ == "__main__":
@@ -23,14 +24,14 @@ if __name__ == "__main__":
         jsondatafile = unzip_file(data_dir + datafile, data_dir)
         jdfile_name = data_dir + jsondatafile[0].filename
 
-    q1_memory_optimize = q1_memory(jdfile_name)
-    print(q1_memory_optimize)
-    q1_time_optimize = q1_time(jdfile_name)
-    print(q1_time_optimize)
+    #q1_memory_optimize = q1_memory(jdfile_name)
+    #print(q1_memory_optimize)
+    #q1_time_optimize = q1_time(jdfile_name)
+    #print(q1_time_optimize)
     q2_memory_optimize = q2_memory(jdfile_name)
     print (q2_memory_optimize)
+    q2_time_optimize = q2_time(jdfile_name)
+    print(q2_time_optimize)
 
 
 
-
-  
--- src/q2_time.py
+++ src/q2_time.py
@@ -1,4 +1,32 @@
 from typing import List, Tuple
+from collections import Counter
+from Readjson import read_json
+from memory_profiler import profile
+import re
+import multiprocessing
+from functools import partial
+
+def process_tweet(tweet, emoji_pattern):
+    text = tweet.get('content', '')
+    emojis = emoji_pattern.findall(text)
+    return emojis
+
 
 def q2_time(file_path: str) -> List[Tuple[str, int]]:
-    pass
+    data = read_json(file_path)
+    emoji_counter = Counter()
+
+    emoji_pattern = re.compile(
+        r'[\U0001F300-\U0001F6FF]|[\U0001F900-\U0001F9FF]|[\U0001F600-\U0001F64F]|[\U0001F680-\U0001F6FF]')
+
+    with multiprocessing.Pool() as pool:
+        process_tweet_partial = partial(process_tweet, emoji_pattern=emoji_pattern)
+        results = pool.map(process_tweet_partial, data)
+
+    for emojis in results:
+        for emoji in emojis:
+            emoji_counter[emoji] += 1
+
+    top_10 = emoji_counter.most_common(10)
+
+    return top_10
