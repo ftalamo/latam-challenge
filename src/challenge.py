542324dc6d0a38288f7d42afadbdf2ded07fb565
ftalamo <francotalamo@gmail.com>
2024-03-10 16:42:16 -0300

    create q1_memory optimized function

--- src/challenge.py
+++ src/challenge.py
@@ -3,21 +3,28 @@ from getdata import getdata
 from unzip import unzip_file
 from q1_memory import q1_memory
 import q1_time
-import q2_memory
 import q2_time
-import q3_memory
+from q2_memory import q2_memory
+from q3_memory import q3_memory
 import q3_time
 
 
 if __name__ == "__main__":
-    """" lee el directorio data y verifica la existencia del archivo .zip """
-    directory = 'data/'
-    name = 'tweets.json.zip'
-    path = directory+name
-    if getdata(directory, name):
+    """" define los datos para enviar a google drive y descargar la data """
+    credentials = 'credentials_module.json'
+    data_dir = 'data/'
+    file_id = '1ig2ngoXFTxP5Pa8muXo02mDTFexZzsis'
+
+    datafile = getdata (file_id, credentials, data_dir)
+
+    if datafile:
         """" descomprime el archivo procede a leerlo y hacer el challenge"""
-        jfile = unzip_file(path, directory)
-        q1_memory(directory+jfile[0].filename)
+        jsondatafile = unzip_file(data_dir+datafile, data_dir)
+        jdfile_name = data_dir+jsondatafile[0].filename
+        q1_memory_optimize = q1_memory(jdfile_name)
+        print(q1_memory_optimize)
+      #  q2_memory(jdfile_name)
+      #  q3_memory(jdfile_name)
 
     else:
         """" si no existe proporciona el link de descarga"""
--- src/q1_memory.py
+++ src/q1_memory.py
@@ -1,27 +1,23 @@
-from typing import List, Tuple
+from typing import  List, Tuple
 from collections import defaultdict
 from datetime import datetime
 from Readjson import read_json
-from memory_profiler import profile
-from datetime import datetime
 from format_date import format_date
 
-@profile()
+
 def q1_memory(file_path: str) -> List[Tuple[datetime.date, str]]:
     data = read_json(file_path)
-    date_counts = defaultdict(int)
-    user_counts = defaultdict(lambda: defaultdict(int))
+    date_user_counts = defaultdict(lambda: defaultdict(int))
+
     for tweet in data:
         date = format_date(tweet['date'])
         username = tweet['user']['username']
-        date_counts[date] += 1
-        user_counts[date][username] += 1
+        date_user_counts[date][username] += 1
 
     # Obtener las 10 fechas principales
-    top_dates = sorted(date_counts.items(), key=lambda x: x[1], reverse=True)[:10]
+    top_dates = sorted(date_user_counts.keys(), key=lambda date: sum(date_user_counts[date].values()), reverse=True)[:10]
 
     # Obtener el usuario con más publicaciones para cada fecha principal
-    result = [(date, max(user_counts[date], key=user_counts[date].get)) for date, _ in top_dates]
-    print(result)
-    pass
+    result = [(date, max(date_user_counts[date], key=date_user_counts[date].get)) for date in top_dates]
 
+    return result
