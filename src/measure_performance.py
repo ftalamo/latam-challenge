eb7f8bf93596e306210b2f0742b53ea257c9bbf8
ftalamo <francotalamo@gmail.com>
2024-03-10 20:15:43 -0300

    add encoding

--- src/measure_performance.py
+++ src/measure_performance.py
@@ -1,8 +1,11 @@
 import psutil
 import timeit
 from datetime import datetime
+from functools import wraps
+
 
 def measure_performance(func):
+    @wraps(func)
     def wrapper(*args, **kwargs):
         start_time = timeit.default_timer()
         result = func(*args, **kwargs)
@@ -14,12 +17,10 @@ def measure_performance(func):
         memory_usage = format(process.memory_info().rss / (1024 * 1024),".4f")  # Convertir a MB
 
         # Guardar los resultados en un archivo
-        with open("performance.txt", "a") as file:
+        with open("performance.txt", "a", encoding="utf-8") as file:
             file.write('**********************************************\n\n')
             file.write(f"Fecha y hora de ejecución: {execution_time}\n")
             file.write(f"Función: {func.__name__}, Tiempo: {elapsed_time} segundos, Uso de memoria: {memory_usage} Mb\n Resultado: {result}\n\n")
-            file.close()
-
         return result
     return wrapper
 
