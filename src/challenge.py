85669ab020c9ecf31deff8b36a0cf0e5e81f09e9
ftalamo <francotalamo@gmail.com>
2024-03-10 17:18:21 -0300

    refator del codigo para descargar el archivo de datos solo si no existe

--- src/challenge.py
+++ src/challenge.py
@@ -1,8 +1,9 @@
+import os
 from getdata import getdata
 from unzip import unzip_file
 from q1_memory import q1_memory
 from q1_time import q1_time
-
+from q2_memory import q2_memory
 
 
 if __name__ == "__main__":
@@ -11,19 +12,25 @@ if __name__ == "__main__":
     data_dir = 'data/'
     file_id = '1ig2ngoXFTxP5Pa8muXo02mDTFexZzsis'
 
-    datafile = getdata (file_id, credentials, data_dir)
+    datafile = 'data/farmers-protest-tweets-2021-2-4.json'
 
-    if datafile:
-        """" descomprime el archivo procede a leerlo y hacer el challenge"""
-        jsondatafile = unzip_file(data_dir+datafile, data_dir)
-        jdfile_name = data_dir+jsondatafile[0].filename
-        q1_memory_optimize = q1_memory(jdfile_name)
-        print(q1_memory_optimize)
-        q1_time_optimize = q1_time(jdfile_name)
-        print(q1_time_optimize)
-      #  q2_memory(jdfile_name)
-      #  q3_memory(jdfile_name)
-
+    if os.path.exists(datafile):
+        '''si existe el archivo copia el nombre'''
+        jdfile_name = datafile
     else:
-        """" si no existe proporciona el link de descarga"""
-        print('Go to https://drive.google.com/file/d/1ig2ngoXFTxP5Pa8muXo02mDTFexZzsis/view?usp=sharing in order to download file')
+        '''descarga el archivo y lo descomprime desde drive'''
+        datafile = getdata(file_id, credentials, data_dir)
+        jsondatafile = unzip_file(data_dir + datafile, data_dir)
+        jdfile_name = data_dir + jsondatafile[0].filename
+
+    q1_memory_optimize = q1_memory(jdfile_name)
+    print(q1_memory_optimize)
+    q1_time_optimize = q1_time(jdfile_name)
+    print(q1_time_optimize)
+    q2_memory_optimize = q2_memory(jdfile_name)
+    print (q2_memory_optimize)
+
+
+
+
+  
